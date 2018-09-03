de78abe8db4906c8018619af9d99dc474616e5cd
faw615 <faw615@mail.usask.ca>
2018-09-03 11:54:08 -0400

    Checkio updata

---
+++ Checkio/three-words.py
@@ -0,0 +1,19 @@
+def checkio(words: str) -> bool:
+    counter = 0
+    for w in words.split(' '):
+        if w.isalpha():
+            counter += 1
+        else:
+            counter = 0
+        if counter == 3:
+             return True
+    return False
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio("Hello World hello") == True, "Hello"
+    assert checkio("He is 123 man") == False, "123 man"
+    assert checkio("1 2 3 4") == False, "Digits"
+    assert checkio("bla bla bla bla") == True, "Bla Bla"
+    assert checkio("Hi") == False, "Hi"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
